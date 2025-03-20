6a565839231c2e89ad0acd071d3f084bf6d98d76
ParkMinSung2580 <114042812+ParkMinSung2580@users.noreply.github.com>
2025-03-20 12:49:02 +0900

    test2

--- GitStudy/Program.cs
+++ GitStudy/Program.cs
@@ -4,9 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("이름을 입력해주세요 : ") ;
+            Console.WriteLine("안녕하세요!");
+            Console.WriteLine("새로운 기능 추가");
+            /*Console.Write("이름을 입력해주세요 : ") ;
             string input = Console.ReadLine();
-            Console.WriteLine("입력하기 이름은 {0} 입니다",input);
+            Console.WriteLine("입력하기 이름은 {0} 입니다",input);*/
         }
     }
 }
